960e5b61ca650b5a782ef4b9491a86472a1e0f16
DELL <anishbhaktasuwal@gmail.com>
2022-05-29 20:58:58 +0545

    first commit

---
+++ src/components/ui/HeroSection.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./hero-section.css";
+
+import heroImg from "../../assets/images/hero.jpg";
+
+const HeroSection = () => {
+  return (
+    <section className="hero__section">
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="hero__content">
+              <h2>
+                Discover rare digital art and collect
+                <span>sell extraordinary</span> NFTs
+              </h2>
+              <p className="hero__btns">
+              <strong>ATB </strong> is the world's one of the finest and largest NFT marketplace.
+              People are buying and selling NFTs today and 99% of the people in this world are not aware of this billion-dollar market and this opportunity is really a once in a century opportunity.
+              </p>
+
+              <div className="hero__btns d-flex align-items-center gap-4">
+                <button className=" explore__btn d-flex align-items-center gap-2">
+                  <i className="ri-rocket-line"></i>{" "}
+                  <Link to="/market">Explore</Link>
+                </button>
+                <button className=" create__btn d-flex align-items-center gap-2">
+                  <i className="ri-ball-pen-line"></i>
+                  <Link to="/create">Create</Link>
+                </button>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="hero__img">
+              <img src={heroImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ src/pages/Wallet.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import CommonSection from "../components/ui/Common-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
+
+import "../styles/wallet.css";
+
+const wallet__data = [
+  {
+    title: "Bitcoin",
+    desc: "Connect us with Bitcoin",
+    icon: "ri-bit-coin-line",
+  },
+
+  {
+    title: "Coinbase",
+    desc: "Connect us with Coinbase",
+    icon: "ri-coin-line",
+  },
+
+  {
+    title: "Metamask",
+    desc: "Connect us with Metamask",
+    icon: "ri-money-cny-circle-line",
+  },
+
+  {
+    title: "Authereum",
+    desc: "Connect us with Authereum",
+    icon: "ri-bit-coin-line",
+  },
+];
+
+const Wallet = () => {
+  return (
+    <>
+      <CommonSection title="Connect Wallet" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+              <div className="w-50 m-auto">
+                <h3 className="text-light">Connect your wallet</h3>
+               
+              </div>
+            </Col>
+
+            {wallet__data.map((item, index) => (
+              <Col lg="3" md="4" sm="6" key={index} className="mb-4">
+                <div className="wallet__item">
+                  <span>
+                    <i className={item.icon}></i>
+                  </span>
+                  <h5>{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Wallet;
